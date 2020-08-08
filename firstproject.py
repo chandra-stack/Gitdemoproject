b175186fb146ed0991e3f4e275c8c621d97abc3d
chandra-stack <chandraiahmo@gmail.com>
2020-08-08 12:48:21 +0530

    american team

--- firstproject.py
+++ firstproject.py
@@ -4,6 +4,7 @@ class Chandra:
         self.a = a
         self.b = b
         print("i will be calling when object created")
+        print("i will come now")
 
     def sum(self):
         return self.a + self.b
